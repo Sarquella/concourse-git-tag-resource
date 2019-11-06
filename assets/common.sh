d23b1b75d41463c8b6b374a52a4e5d382603a8f7
Adrià Sarquella <adria@sarquella.email>
2019-11-06 11:32:57 +0100

    Git metadata added in  script output

--- assets/common.sh
+++ assets/common.sh
@@ -149,3 +149,96 @@ format_output() {
 
   echo "[$output]" | sed "s/}{/},{/g"
 }
+
+add_git_metadata_basic() {
+  local commit=$(git rev-parse HEAD | jq -R .)
+  local author=$(git log -1 --format=format:%an | jq -s -R .)
+  local author_date=$(git log -1 --format=format:%ai | jq -R .)
+
+  jq ". + [
+    {name: \"commit\", value: ${commit}},
+    {name: \"author\", value: ${author}},
+    {name: \"author_date\", value: ${author_date}, type: \"time\"}
+  ]"
+}
+
+add_git_metadata_committer() {
+  local author=$(git log -1 --format=format:%an | jq -s -R .)
+  local author_date=$(git log -1 --format=format:%ai | jq -R .)
+  local committer=$(git log -1 --format=format:%cn | jq -s -R .)
+  local committer_date=$(git log -1 --format=format:%ci | jq -R .)
+
+  if [ "$author" = "$committer" ] && [ "$author_date" = "$committer_date" ]; then
+    jq ". + [
+      {name: \"committer\", value: ${committer}},
+      {name: \"committer_date\", value: ${committer_date}, type: \"time\"}
+    ]"
+  else
+    cat
+  fi
+}
+
+add_git_metadata_branch() {
+  local branch=$(git show-ref --heads | \
+    sed -n "s/^$(git rev-parse HEAD) refs\/heads\/\(.*\)/\1/p" |  \
+    jq -R  ". | select(. != \"\")" | jq -r -s "map(.) | join (\",\")")
+
+  if [ -n "${branch}" ]; then
+    jq ". + [
+      {name: \"branch\", value: \"${branch}\"}
+    ]"
+  else
+    cat
+  fi
+}
+
+add_git_metadata_tags() {
+  local tags=$(git tag --points-at HEAD | \
+    jq -R  ". | select(. != \"\")" | \
+    jq -r -s "map(.) | join(\",\")")
+
+  if [ -n "${tags}" ]; then
+    jq ". + [
+      {name: \"tags\", value: \"${tags}\"}
+    ]"
+  else
+    cat
+  fi
+}
+
+add_git_metadata_message() {
+  local message=$(git log -1 --format=format:%B | jq -s -R .)
+
+  jq ". + [
+    {name: \"message\", value: ${message}, type: \"message\"}
+  ]"
+}
+
+add_git_metadata_url() {
+  local commit=$(git rev-parse HEAD)
+  local origin=$(git remote get-url --all origin) 2> /dev/null
+
+  if echo $origin | grep github.com > /dev/null; then
+
+    # git@github.com:concourse/git-resource.git     -> concourse/git-resource
+    # https://github.com/concourse/git-resource.git -> concourse/git-resource
+    local ownerRepo=$(echo $origin | sed -e' s/.*github.com[:\/]//; s/\.git$//')
+    local url=$(echo "https://github.com/$ownerRepo/commit/$commit" | jq -R . )
+
+    jq ". + [
+        {name: \"url\", value: ${url}}
+    ]"
+  else
+    jq ". + []"
+  fi
+}
+
+git_metadata() {
+  jq -n "[]" | \
+    add_git_metadata_basic | \
+    add_git_metadata_committer | \
+    add_git_metadata_branch | \
+    add_git_metadata_tags | \
+    add_git_metadata_message | \
+    add_git_metadata_url
+}
