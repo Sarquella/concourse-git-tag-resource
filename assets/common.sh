bc2355c485ef59e084f0d94fd6b9fb74f568dca8
Adrià Sarquella <adria@sarquella.email>
2019-11-06 11:48:58 +0100

    Cloning repo in quiet mode

--- assets/common.sh
+++ assets/common.sh
@@ -80,12 +80,12 @@ clone_repo() {
   local clone_flags=$2
 
   if [ ! -d "$destination/.git" ]; then
-    log "Cloning $uri in $destination"
+    log "Cloning $uri into $destination"
 
     git clone $clone_flags "$uri" "$destination"
     cd $destination
   else
-    log "Reseting $uri in $destination"
+    log "Reseting $uri into $destination"
 
     cd $destination
     git reset --hard FETCH_HEAD
